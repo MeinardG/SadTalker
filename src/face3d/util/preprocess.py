68cf3c264869f5f1695660c3b25e8c3a66107463
Meinardo González <meinardg@gmail.com>
2025-12-22 22:21:39 -0600

    Updated to use Torch 2.9 + Cuda 13

--- src/face3d/util/preprocess.py
+++ src/face3d/util/preprocess.py
@@ -2,6 +2,7 @@
 """
 
 import numpy as np
+from typing import Union, Any
 from scipy.io import loadmat
 from PIL import Image
 import cv2
@@ -70,6 +71,12 @@ def extract_5p(lm):
     lm5p = lm5p[[1, 2, 0, 3, 4], :]
     return lm5p
 
+def maybe_single_item(array: np.ndarray) -> Union[np.ndarray, Any]:
+    try:
+        return array.item()
+    except TypeError:
+        return array
+
 # utils for face reconstruction
 def align_img(img, lm, lm3D, mask=None, target_size=224., rescale_factor=102.):
     """
@@ -98,6 +105,6 @@ def align_img(img, lm, lm3D, mask=None, target_size=224., rescale_factor=102.):
 
     # processing the image
     img_new, lm_new, mask_new = resize_n_crop_img(img, lm, t, s, target_size=target_size, mask=mask)
-    trans_params = np.array([w0, h0, s, t[0], t[1]])
+    trans_params = np.array([w0, h0, s, maybe_single_item(t[0]), maybe_single_item(t[1])])
 
     return trans_params, img_new, lm_new, mask_new
